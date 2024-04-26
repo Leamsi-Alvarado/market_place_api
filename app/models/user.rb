0ea66a9683468525d58afd3dde8ddf0e6e07a2a2
Leamsi Alvarado <leamsi-a-alvarado-g@outlook.com>
2024-04-26 12:43:19 -0600

    generate product model and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  #Validations
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
-
+  #Password hash
   has_secure_password
+
+  #Relations with other models
+  has_many :products, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_25_223922) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_26_182407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "published"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -22,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_25_223922) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
