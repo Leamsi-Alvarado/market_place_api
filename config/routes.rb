9b6a342f8e43c5aa2249e05ba65622255ce0f395
Leamsi Alvarado <leamsi@MacBook-Air-de-Leamsi.local>
2024-04-25 20:03:37 -0600

    Added show action to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :api, default: {format: :json} do
-    namespace :vi do
-    #We are going to list our resources here
-
+    namespace :v1 do
+      resources :users, only: [:show]
     end
   end
 end
