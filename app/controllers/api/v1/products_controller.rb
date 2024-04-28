3c6b880d1fad369b366b17eca593c0cdf2caaf58
Leamsi Alvarado <leamsi-a-alvarado-g@outlook.com>
2024-04-28 16:09:51 -0600

    adds products serializer for json response

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show update destroy]
+  before_action :check_login, only: %i[create]
+  before_action :check_owner, only: %i[update destroy]
+
+  def destroy
+    @product.destroy
+    head 204
+  end
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: ProductSerializer.new(@product).serializable_hash, status: :created
+    else
+      render json: {errors: product.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render json: ProductSerializer.new(@product).serializable_hash
+  end
+
+  def index
+    @products =  Product.all
+    render json: ProductSerializer.new(@products).serializable_hash
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: ProductSerializer.new(@product).serializable_hash, status: :created
+    else
+      render errors: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
+  end
+
+  def check_owner
+    head :forbidden unless @product.user_id == current_user&.id
+  end
+  def set_product
+    @product = Product.find(params[:id])
+  end
+end
