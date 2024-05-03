e314f59cdd1fd755475a48f7b0948ffe05f7e9a1
Leamsi-Alvarado <leamsi-a-alvarado-g@outlook.com>
2024-05-02 20:48:50 -0600

    Decreases the product quantity by the placement quantity0

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,11 @@
 class Placement < ApplicationRecord
   belongs_to :order
   belongs_to :product
+  after_create :decrement_product_quantity!
+
+  def decrement_product_quantity!
+    if product && quantity.present?
+      product.decrement!(:quantity, quantity)
+    end
+  end
 end
