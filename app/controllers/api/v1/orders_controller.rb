fbf2027c01a644c76ce07dfc680a130862169dce
Leamsi-Alvarado <leamsi-a-alvarado-g@outlook.com>
2024-05-02 19:55:31 -0600

    Allos the order to be placed along with quantity

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -17,7 +17,8 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def create
-    order = current_user.orders.build(order_params)
+    order = Order.create! user: current_user
+    order.build_placements_with_product_ids_and_quantities(order_params[:product_ids_and_quantities])
     if order.save
       OrderMailer.send_confirmation(order).deliver
       render json: order, status: 201
@@ -26,4 +27,10 @@ class Api::V1::OrdersController < ApplicationController
     end
   end
 
+  private
+  def order_params
+    params.require(:order).permit(product_ids_and_quantities: [:product_id, :quantity])
+  end
+
 end
+
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -3,7 +3,16 @@ require "test_helper"
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @order = orders(:one)
+    @order_params = {
+      order: {
+        product_ids_and_quantities: [
+          { product_id: products(:one).id, quantity: 2 },
+          { product_id: products(:two).id, quantity: 3 },
+        ]
+      }
+    }
   end
+
 
   test 'should forbid order for unlogged' do
     get api_v1_orders_url, as: :json
@@ -11,7 +20,7 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show orders' do
-    get api_v1_orders_url, headers: {Authorization: JsonWebToken.encode(user_id: @order.user_id)}, as: :json
+    get api_v1_orders_url, headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) }, as: :json
     assert_response :success
 
     json_response = JSON.parse(response.body)
@@ -19,11 +28,22 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show order' do
-    get api_v1_order_url(@order), headers: {Authorization: JsonWebToken.encode(user_id: @order.user_id)}, as: :json
+    get api_v1_order_url(@order), headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) }, as: :json
     assert_response :success
 
     json_response = JSON.parse(response.body)
     include_product_attr = json_response['included'][0]['attributes']
     assert_equal @order.products.first.title, include_product_attr['title']
+  end
+
+
+  test 'should create order with two products and placements' do
+    assert_difference('Order.count', 1) do
+      assert_difference('Placement.count', 2) do
+        post api_v1_orders_url, params: @order_params, as: :json,
+        headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) }
+      end
+    end
+    assert_response :created
   end
 end
